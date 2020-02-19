19fe4cb8ff07c78237f88316b9f358be618bc1cb
Pepijnnn <32436117+Pepijnnn@users.noreply.github.com>
2020-02-19 12:34:56 +0100

    view2 added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,25 @@
-from flask import render_template
+from flask import render_template, request, jsonify
+from bokeh.plotting import output_file
 from app import app
+from app import images
+import os, json
+
+from decimal import Decimal
+
+import pandas as pd
+import numpy as np
+
 
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Welcome!')
+
+@app.route("/view2")
+def view2():
+	output_file('view2.html')
+	plot = images.show_img()
+	jsonify(plotData=plot)
+	return render_template('view2.html')
+	# return jsonify(plotData=plot)
---
+++ app/static/js/show_image.js
@@ -0,0 +1,7 @@
+// <!--data/230_works_1024x/Access_2007.jpg-->
+var img = document.createElement("img");
+ 
+img.src = "../data/230_works_1024x/Access_2007.jpg";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
